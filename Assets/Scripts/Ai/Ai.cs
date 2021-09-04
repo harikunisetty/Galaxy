744f9f1c8f186edbbfc88d9091fccbc0be70aa4d
harikunisetty <86394723+harikunisetty@users.noreply.github.com>
2021-09-04 15:54:33 +0530

    update

--- Assets/Scripts/Ai/Ai.cs
+++ Assets/Scripts/Ai/Ai.cs
@@ -6,6 +6,7 @@ public class Ai : MonoBehaviour
 {
     public Transform player;
     public float lifetime;
+    [SerializeField] float zAngle;
     void Start()
     {
        
@@ -22,6 +23,8 @@ public class Ai : MonoBehaviour
         }
         if (player == null)
             return;
-        Vector3 dir = player.position - transform.position; 
+        Vector3 dir = player.position - transform.position;
+        zAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
+        transform.rotation = Quaternion.Euler(0f, 0f, zAngle);
     }
 }
